44a5747dee185dcab91b3c351acd6012cbd4ac4c
therealilyaskhan <therealilyaskhan@hotmail.com>
2023-09-17 00:30:36 +0500

    javascript updated for smooth loading of newly fetched products

--- assets/theme.js
+++ assets/theme.js
@@ -3626,8 +3626,8 @@ var ProductList = class extends HTMLElement {
         }
       });
     });
-    console.log(this);
-    observer.observe(this.querySelector('product-list'), { attributes: true, subtree: false, childList: true });
+    
+    observer.observe(document.querySelector('product-list'), { attributes: true, subtree: false, childList: true });
     // mik added custom javascript for smooth animation when new childnodes are added to reveal-items elements
 
   }
